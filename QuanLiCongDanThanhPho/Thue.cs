d103fce5c5d676b6dc1f6e2365bae61ab67ba766
Van Duy <vanduy352003@gmail.com>
2023-02-28 15:29:42 +0700

    them class thue

---
+++ QuanLiCongDanThanhPho/Thue.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLiCongDanThanhPho
+{
+    internal class Thue
+    {
+        private string maThue;
+        private string coQuanQuanLy;
+        private DateTime ngayCapMa;
+        private DateTime hanNop;
+        private string soTien;
+
+        public string MaThue { get { return maThue; } }
+        public string CoQuanQuanLy { get { return coQuanQuanLy; } }
+        public DateTime HanNop { get { return hanNop; } }
+        public string SoTien { get { return soTien; } }
+        public DateTime NgayCapMa { get { return ngayCapMa; } }
+    }
+}
