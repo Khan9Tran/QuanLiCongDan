57df1e79b44c2076f43ba5da93b37d3721b612cb
Van Duy <vanduy352003@gmail.com>
2023-02-28 15:14:51 +0700

    Them class dia chi

---
+++ QuanLiCongDanThanhPho/DiaChi.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLiCongDanThanhPho
+{
+    internal class DiaChi
+    {
+        private string soDiaChi;
+        private string soNha;
+        private string duong;
+        private string quanHuyen;
+        private string tinhThanhPho;
+        private string quocGia;
+
+        public string SoDiaChi { get { return soDiaChi; } }
+        public string SoNha { get { return soNha; } }
+        public string Duong { get { return duong; } }
+        public string QuanHuyen { get { return quanHuyen; } }
+        public string TinhThanhPho { get { return tinhThanhPho; } }
+        public string QuocGia { get { return quocGia; } }
+
+        public string ToString()
+        {
+            string diaChi = $"{SoDiaChi}, {SoNha}, {Duong}, {QuanHuyen}, {TinhThanhPho}, {QuocGia}";
+            return diaChi;
+        }
+    }
+}
