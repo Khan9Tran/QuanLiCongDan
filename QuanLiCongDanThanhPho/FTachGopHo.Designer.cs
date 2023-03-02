1291c918bb9f75ed17a874a2ee4742aaec763d79
Van Duy <vanduy352003@gmail.com>
2023-03-02 18:29:16 +0700

    Them phan form dang ky ho khau

---
+++ QuanLiCongDanThanhPho/FTachGopHo.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace QuanLiCongDanThanhPho
+{
+    partial class FTachGopHo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMaHoTach = new System.Windows.Forms.Label();
+            this.lblMaHoGop = new System.Windows.Forms.Label();
+            this.gvHoTach = new System.Windows.Forms.DataGridView();
+            this.gvHoGop = new System.Windows.Forms.DataGridView();
+            this.btnTach = new System.Windows.Forms.Button();
+            this.btnGopHo = new System.Windows.Forms.Button();
+            this.btnTaoHoMoi = new System.Windows.Forms.Button();
+            this.txtMaHoTach = new System.Windows.Forms.TextBox();
+            this.txtMaHoGop = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.gvHoTach)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gvHoGop)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblMaHoTach
+            // 
+            this.lblMaHoTach.AutoSize = true;
+            this.lblMaHoTach.Location = new System.Drawing.Point(95, 82);
+            this.lblMaHoTach.Name = "lblMaHoTach";
+            this.lblMaHoTach.Size = new System.Drawing.Size(119, 28);
+            this.lblMaHoTach.TabIndex = 4;
+            this.lblMaHoTach.Text = "Mã hộ tách";
+            this.lblMaHoTach.Click += new System.EventHandler(this.lblMaHoTach_Click);
+            // 
+            // lblMaHoGop
+            // 
+            this.lblMaHoGop.AutoSize = true;
+            this.lblMaHoGop.Location = new System.Drawing.Point(838, 83);
+            this.lblMaHoGop.Name = "lblMaHoGop";
+            this.lblMaHoGop.Size = new System.Drawing.Size(114, 28);
+            this.lblMaHoGop.TabIndex = 5;
+            this.lblMaHoGop.Text = "Mã hộ gộp";
+            // 
+            // gvHoTach
+            // 
+            this.gvHoTach.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gvHoTach.Location = new System.Drawing.Point(95, 176);
+            this.gvHoTach.Name = "gvHoTach";
+            this.gvHoTach.RowHeadersWidth = 51;
+            this.gvHoTach.RowTemplate.Height = 29;
+            this.gvHoTach.Size = new System.Drawing.Size(590, 425);
+            this.gvHoTach.TabIndex = 6;
+            // 
+            // gvHoGop
+            // 
+            this.gvHoGop.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gvHoGop.Location = new System.Drawing.Point(838, 176);
+            this.gvHoGop.Name = "gvHoGop";
+            this.gvHoGop.RowHeadersWidth = 51;
+            this.gvHoGop.RowTemplate.Height = 29;
+            this.gvHoGop.Size = new System.Drawing.Size(590, 425);
+            this.gvHoGop.TabIndex = 7;
+            // 
+            // btnTach
+            // 
+            this.btnTach.Location = new System.Drawing.Point(447, 623);
+            this.btnTach.Name = "btnTach";
+            this.btnTach.Size = new System.Drawing.Size(150, 50);
+            this.btnTach.TabIndex = 10;
+            this.btnTach.Text = "Tách hộ";
+            this.btnTach.UseVisualStyleBackColor = true;
+            // 
+            // btnGopHo
+            // 
+            this.btnGopHo.Location = new System.Drawing.Point(689, 623);
+            this.btnGopHo.Name = "btnGopHo";
+            this.btnGopHo.Size = new System.Drawing.Size(150, 50);
+            this.btnGopHo.TabIndex = 11;
+            this.btnGopHo.Text = "Gộp hộ";
+            this.btnGopHo.UseVisualStyleBackColor = true;
+            // 
+            // btnTaoHoMoi
+            // 
+            this.btnTaoHoMoi.Location = new System.Drawing.Point(937, 623);
+            this.btnTaoHoMoi.Name = "btnTaoHoMoi";
+            this.btnTaoHoMoi.Size = new System.Drawing.Size(150, 50);
+            this.btnTaoHoMoi.TabIndex = 12;
+            this.btnTaoHoMoi.Text = "Tạo hộ mới";
+            this.btnTaoHoMoi.UseVisualStyleBackColor = true;
+            // 
+            // txtMaHoTach
+            // 
+            this.txtMaHoTach.BackColor = System.Drawing.Color.Gainsboro;
+            this.txtMaHoTach.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtMaHoTach.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtMaHoTach.Location = new System.Drawing.Point(232, 83);
+            this.txtMaHoTach.Name = "txtMaHoTach";
+            this.txtMaHoTach.Size = new System.Drawing.Size(442, 27);
+            this.txtMaHoTach.TabIndex = 20;
+            // 
+            // txtMaHoGop
+            // 
+            this.txtMaHoGop.BackColor = System.Drawing.Color.Gainsboro;
+            this.txtMaHoGop.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtMaHoGop.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtMaHoGop.Location = new System.Drawing.Point(958, 83);
+            this.txtMaHoGop.Name = "txtMaHoGop";
+            this.txtMaHoGop.Size = new System.Drawing.Size(470, 27);
+            this.txtMaHoGop.TabIndex = 21;
+            // 
+            // FTachGopHo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 28F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1532, 695);
+            this.Controls.Add(this.txtMaHoGop);
+            this.Controls.Add(this.txtMaHoTach);
+            this.Controls.Add(this.btnTaoHoMoi);
+            this.Controls.Add(this.btnGopHo);
+            this.Controls.Add(this.btnTach);
+            this.Controls.Add(this.gvHoGop);
+            this.Controls.Add(this.gvHoTach);
+            this.Controls.Add(this.lblMaHoGop);
+            this.Controls.Add(this.lblMaHoTach);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "FTachGopHo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FTachGopHo";
+            ((System.ComponentModel.ISupportInitialize)(this.gvHoTach)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gvHoGop)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label lblMaHoTach;
+        private Label lblMaHoGop;
+        private DataGridView gvHoTach;
+        private DataGridView gvHoGop;
+        private Button btnTach;
+        private Button btnGopHo;
+        private Button btnTaoHoMoi;
+        private TextBox txtMaHoTach;
+        private TextBox txtMaHoGop;
+    }
+}
---
+++ QuanLiCongDanThanhPho/FThemNguoiVaoHo.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace QuanLiCongDanThanhPho
+{
+    partial class FThemNguoiVaoHo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMaHo = new System.Windows.Forms.TextBox();
+            this.lblMaHo = new System.Windows.Forms.Label();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTen = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCCCD = new System.Windows.Forms.TextBox();
+            this.btnTim = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtMaHo
+            // 
+            this.txtMaHo.BackColor = System.Drawing.Color.Gainsboro;
+            this.txtMaHo.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtMaHo.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtMaHo.Location = new System.Drawing.Point(546, 180);
+            this.txtMaHo.Name = "txtMaHo";
+            this.txtMaHo.Size = new System.Drawing.Size(442, 27);
+            this.txtMaHo.TabIndex = 22;
+            // 
+            // lblMaHo
+            // 
+            this.lblMaHo.AutoSize = true;
+            this.lblMaHo.Location = new System.Drawing.Point(409, 179);
+            this.lblMaHo.Name = "lblMaHo";
+            this.lblMaHo.Size = new System.Drawing.Size(78, 28);
+            this.lblMaHo.TabIndex = 21;
+            this.lblMaHo.Text = "Mã hộ ";
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(641, 453);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(197, 50);
+            this.btnThem.TabIndex = 23;
+            this.btnThem.Text = "Thêm thành viên";
+            this.btnThem.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(409, 269);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 28);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "Tên";
+            // 
+            // txtTen
+            // 
+            this.txtTen.BackColor = System.Drawing.Color.Gainsboro;
+            this.txtTen.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtTen.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtTen.Location = new System.Drawing.Point(546, 270);
+            this.txtTen.Name = "txtTen";
+            this.txtTen.Size = new System.Drawing.Size(442, 27);
+            this.txtTen.TabIndex = 25;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(409, 350);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 28);
+            this.label2.TabIndex = 26;
+            this.label2.Text = "CCCD";
+            // 
+            // txtCCCD
+            // 
+            this.txtCCCD.BackColor = System.Drawing.Color.Gainsboro;
+            this.txtCCCD.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtCCCD.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtCCCD.Location = new System.Drawing.Point(546, 350);
+            this.txtCCCD.Name = "txtCCCD";
+            this.txtCCCD.Size = new System.Drawing.Size(292, 27);
+            this.txtCCCD.TabIndex = 27;
+            // 
+            // btnTim
+            // 
+            this.btnTim.Location = new System.Drawing.Point(878, 345);
+            this.btnTim.Name = "btnTim";
+            this.btnTim.Size = new System.Drawing.Size(110, 39);
+            this.btnTim.TabIndex = 28;
+            this.btnTim.Text = "Tìm";
+            this.btnTim.UseVisualStyleBackColor = true;
+            // 
+            // FThemNguoiVaoHo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 28F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1532, 695);
+            this.Controls.Add(this.btnTim);
+            this.Controls.Add(this.txtCCCD);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTen);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.txtMaHo);
+            this.Controls.Add(this.lblMaHo);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FThemNguoiVaoHo";
+            this.Text = "FThemNguoiVaoHo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox txtMaHo;
+        private Label lblMaHo;
+        private Button btnThem;
+        private Label label1;
+        private TextBox txtTen;
+        private Label label2;
+        private TextBox txtCCCD;
+        private Button btnTim;
+    }
+}
