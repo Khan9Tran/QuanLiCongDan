4be41a8ef737f7a20a173aa38a7bdf974cb345d6
Van Duy <vanduy352003@gmail.com>
2023-03-07 13:31:36 +0700

    them do thong tin vao FThongTin HonNhan, Thue, Khai sinh. O trong FThongTinCongDan an nut chi tiet qua duoc

--- QuanLiCongDanThanhPho/FThongTinKhaiSinh.cs
+++ QuanLiCongDanThanhPho/FThongTinKhaiSinh.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QuanLiCongDanThanhPho.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +13,13 @@ namespace QuanLiCongDanThanhPho
 {
     public partial class FThongTinKhaiSinh : Form
     {
+        private string maCCCD;
+        public string MaCCCD
+        {
+            set { maCCCD = value; }
+            get { return maCCCD; }
+        }
+        KhaiSinhDAO ksDAO = new KhaiSinhDAO();
         public FThongTinKhaiSinh()
         {
             InitializeComponent();
@@ -19,20 +27,43 @@ namespace QuanLiCongDanThanhPho
 
         private void btnThongTinCha_Click(object sender, EventArgs e)
         {
-            if (txtCccdCha.Text != "")
+            if (txtCccdCha.Text != "" && txtCccdCha.Text != "u")
             {
                 FThongTinCongDan tTCD = new FThongTinCongDan();
+                tTCD.MaCCCD = txtCccdCha.Text;
                 tTCD.ShowDialog();
             }
         }
 
         private void btnThongTinMe_Click(object sender, EventArgs e)
         {
-            if (txtCccdMe.Text != "")
+            if (txtCccdMe.Text != "" && txtCccdMe.Text != "u")
             {
                 FThongTinCongDan tTCD = new FThongTinCongDan();
+                tTCD.MaCCCD = txtCccdMe.Text;
                 tTCD.ShowDialog();
             }
+        }
+
+        private void FThongTinKhaiSinh_Load(object sender, EventArgs e)
+        {
+            KhaiSinh ks = ksDAO.LayThongTin(maCCCD);
+            KhaiSinh ksCha = ksDAO.LayThongTin(ks.CCCDCha);
+            KhaiSinh ksMe = ksDAO.LayThongTin(ks.CCCDMe);
+            txtTen.Text = ks.HoTen;
+            txtCccd.Text = ks.MaKhaiSinh;
+            txtNoiSinh.Text = ks.NoiSinh.toString();
+            dtpNgaySinh.Value = ks.NgaySinh;
+            txtGioiTinh.Text = ks.GioiTinh;
+            txtDanToc.Text = ks.DanToc;
+            txtQuocTich.Text = ks.QuocTich;
+            txtQueQuan.Text = ks.QueQuan.toString();
+            txtTenCha.Text = ks.TenCha;
+            txtTenMe.Text = ks.TenMe;
+            txtCccdCha.Text = ks.CCCDCha;
+            txtCccdMe.Text = ks.CCCDMe;
+            txtQuocTichCha.Text = ksCha.QuocTich;
+            txtQuocTichMe.Text = ksMe.QuocTich;
         }
     }
 }
