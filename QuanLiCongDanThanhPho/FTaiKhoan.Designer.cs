241ae85bf52f735afe759ef878f745072d8f5e15
Khan9Tran <trankhangtctv@gmail.com>
2023-03-17 22:45:22 +0700

    thêm giao diện

--- QuanLiCongDanThanhPho/FTaiKhoan.Designer.cs
+++ QuanLiCongDanThanhPho/FTaiKhoan.Designer.cs
@@ -28,24 +28,255 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnCapNhat = new System.Windows.Forms.Button();
+            this.btnDoiMK = new System.Windows.Forms.Button();
+            this.txtDisplayName = new System.Windows.Forms.TextBox();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.picAvatar = new System.Windows.Forms.PictureBox();
+            this.btnThemHinh = new System.Windows.Forms.Button();
+            this.pnlDoiPass = new System.Windows.Forms.Panel();
+            this.txtMatKhauMoiNhapLai = new System.Windows.Forms.TextBox();
+            this.txtMatKhauMoi = new System.Windows.Forms.TextBox();
+            this.txtMatKhau = new System.Windows.Forms.TextBox();
+            this.lblNhapLaiMK = new System.Windows.Forms.Label();
+            this.lblMatKhauMoi = new System.Windows.Forms.Label();
+            this.lblMatKhau = new System.Windows.Forms.Label();
+            this.fpnlChucNang = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnDangKy = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picAvatar)).BeginInit();
+            this.pnlDoiPass.SuspendLayout();
+            this.fpnlChucNang.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // btnCapNhat
+            // 
+            this.btnCapNhat.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(75)))), ((int)(((byte)(150)))));
+            this.btnCapNhat.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCapNhat.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnCapNhat.Location = new System.Drawing.Point(152, 623);
+            this.btnCapNhat.Name = "btnCapNhat";
+            this.btnCapNhat.Size = new System.Drawing.Size(181, 52);
+            this.btnCapNhat.TabIndex = 19;
+            this.btnCapNhat.Text = "Cập nhật";
+            this.btnCapNhat.UseVisualStyleBackColor = false;
+            // 
+            // btnDoiMK
+            // 
+            this.btnDoiMK.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(75)))), ((int)(((byte)(150)))));
+            this.btnDoiMK.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDoiMK.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnDoiMK.Location = new System.Drawing.Point(488, 623);
+            this.btnDoiMK.Name = "btnDoiMK";
+            this.btnDoiMK.Size = new System.Drawing.Size(181, 52);
+            this.btnDoiMK.TabIndex = 20;
+            this.btnDoiMK.Text = "Đổi mật khẩu";
+            this.btnDoiMK.UseVisualStyleBackColor = false;
+            // 
+            // txtDisplayName
+            // 
+            this.txtDisplayName.BackColor = System.Drawing.Color.Gainsboro;
+            this.txtDisplayName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtDisplayName.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtDisplayName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(75)))), ((int)(((byte)(150)))));
+            this.txtDisplayName.Location = new System.Drawing.Point(152, 439);
+            this.txtDisplayName.Name = "txtDisplayName";
+            this.txtDisplayName.Size = new System.Drawing.Size(517, 27);
+            this.txtDisplayName.TabIndex = 17;
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.BackColor = System.Drawing.Color.Gainsboro;
+            this.txtUserName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtUserName.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtUserName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(75)))), ((int)(((byte)(150)))));
+            this.txtUserName.Location = new System.Drawing.Point(152, 533);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(517, 27);
+            this.txtUserName.TabIndex = 21;
+            // 
+            // picAvatar
+            // 
+            this.picAvatar.BackColor = System.Drawing.Color.Gainsboro;
+            this.picAvatar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.picAvatar.Location = new System.Drawing.Point(162, 66);
+            this.picAvatar.Name = "picAvatar";
+            this.picAvatar.Size = new System.Drawing.Size(232, 284);
+            this.picAvatar.TabIndex = 22;
+            this.picAvatar.TabStop = false;
+            // 
+            // btnThemHinh
+            // 
+            this.btnThemHinh.BackColor = System.Drawing.Color.Transparent;
+            this.btnThemHinh.FlatAppearance.BorderSize = 0;
+            this.btnThemHinh.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnThemHinh.Image = global::QuanLiCongDanThanhPho.Properties.Resources.upload;
+            this.btnThemHinh.Location = new System.Drawing.Point(362, 370);
+            this.btnThemHinh.Name = "btnThemHinh";
+            this.btnThemHinh.Size = new System.Drawing.Size(32, 32);
+            this.btnThemHinh.TabIndex = 106;
+            this.btnThemHinh.UseVisualStyleBackColor = false;
+            // 
+            // pnlDoiPass
+            // 
+            this.pnlDoiPass.BackColor = System.Drawing.Color.Gainsboro;
+            this.pnlDoiPass.Controls.Add(this.txtMatKhauMoiNhapLai);
+            this.pnlDoiPass.Controls.Add(this.txtMatKhauMoi);
+            this.pnlDoiPass.Controls.Add(this.txtMatKhau);
+            this.pnlDoiPass.Controls.Add(this.lblNhapLaiMK);
+            this.pnlDoiPass.Controls.Add(this.lblMatKhauMoi);
+            this.pnlDoiPass.Controls.Add(this.lblMatKhau);
+            this.pnlDoiPass.Controls.Add(this.fpnlChucNang);
+            this.pnlDoiPass.Location = new System.Drawing.Point(830, 101);
+            this.pnlDoiPass.Name = "pnlDoiPass";
+            this.pnlDoiPass.Size = new System.Drawing.Size(671, 554);
+            this.pnlDoiPass.TabIndex = 107;
+            // 
+            // txtMatKhauMoiNhapLai
+            // 
+            this.txtMatKhauMoiNhapLai.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtMatKhauMoiNhapLai.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtMatKhauMoiNhapLai.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtMatKhauMoiNhapLai.ForeColor = System.Drawing.Color.DimGray;
+            this.txtMatKhauMoiNhapLai.Location = new System.Drawing.Point(286, 307);
+            this.txtMatKhauMoiNhapLai.Name = "txtMatKhauMoiNhapLai";
+            this.txtMatKhauMoiNhapLai.Size = new System.Drawing.Size(344, 27);
+            this.txtMatKhauMoiNhapLai.TabIndex = 110;
+            // 
+            // txtMatKhauMoi
+            // 
+            this.txtMatKhauMoi.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtMatKhauMoi.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtMatKhauMoi.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtMatKhauMoi.ForeColor = System.Drawing.Color.DimGray;
+            this.txtMatKhauMoi.Location = new System.Drawing.Point(286, 207);
+            this.txtMatKhauMoi.Name = "txtMatKhauMoi";
+            this.txtMatKhauMoi.Size = new System.Drawing.Size(344, 27);
+            this.txtMatKhauMoi.TabIndex = 109;
+            // 
+            // txtMatKhau
+            // 
+            this.txtMatKhau.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtMatKhau.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtMatKhau.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtMatKhau.ForeColor = System.Drawing.Color.DimGray;
+            this.txtMatKhau.Location = new System.Drawing.Point(286, 107);
+            this.txtMatKhau.Name = "txtMatKhau";
+            this.txtMatKhau.Size = new System.Drawing.Size(344, 27);
+            this.txtMatKhau.TabIndex = 108;
+            // 
+            // lblNhapLaiMK
+            // 
+            this.lblNhapLaiMK.AutoSize = true;
+            this.lblNhapLaiMK.Location = new System.Drawing.Point(40, 306);
+            this.lblNhapLaiMK.Name = "lblNhapLaiMK";
+            this.lblNhapLaiMK.Size = new System.Drawing.Size(194, 28);
+            this.lblNhapLaiMK.TabIndex = 103;
+            this.lblNhapLaiMK.Text = "Xác nhận mật khẩu";
+            // 
+            // lblMatKhauMoi
+            // 
+            this.lblMatKhauMoi.AutoSize = true;
+            this.lblMatKhauMoi.Location = new System.Drawing.Point(40, 206);
+            this.lblMatKhauMoi.Name = "lblMatKhauMoi";
+            this.lblMatKhauMoi.Size = new System.Drawing.Size(145, 28);
+            this.lblMatKhauMoi.TabIndex = 102;
+            this.lblMatKhauMoi.Text = "Mật khẩu mới";
+            // 
+            // lblMatKhau
+            // 
+            this.lblMatKhau.AutoSize = true;
+            this.lblMatKhau.Location = new System.Drawing.Point(40, 107);
+            this.lblMatKhau.Name = "lblMatKhau";
+            this.lblMatKhau.Size = new System.Drawing.Size(102, 28);
+            this.lblMatKhau.TabIndex = 101;
+            this.lblMatKhau.Text = "Mật khẩu";
+            // 
+            // fpnlChucNang
+            // 
+            this.fpnlChucNang.Controls.Add(this.btnReset);
+            this.fpnlChucNang.Controls.Add(this.btnDangKy);
+            this.fpnlChucNang.Location = new System.Drawing.Point(286, 444);
+            this.fpnlChucNang.Name = "fpnlChucNang";
+            this.fpnlChucNang.Size = new System.Drawing.Size(349, 59);
+            this.fpnlChucNang.TabIndex = 100;
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.Transparent;
+            this.btnReset.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnReset.FlatAppearance.BorderSize = 0;
+            this.btnReset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReset.Image = global::QuanLiCongDanThanhPho.Properties.Resources.reset;
+            this.btnReset.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnReset.Location = new System.Drawing.Point(3, 3);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(150, 50);
+            this.btnReset.TabIndex = 92;
+            this.btnReset.Text = "Tải lại";
+            this.btnReset.UseVisualStyleBackColor = false;
+            // 
+            // btnDangKy
+            // 
+            this.btnDangKy.BackColor = System.Drawing.Color.Transparent;
+            this.btnDangKy.FlatAppearance.BorderSize = 0;
+            this.btnDangKy.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDangKy.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnDangKy.ForeColor = System.Drawing.Color.Black;
+            this.btnDangKy.Image = global::QuanLiCongDanThanhPho.Properties.Resources.confirm;
+            this.btnDangKy.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnDangKy.Location = new System.Drawing.Point(159, 3);
+            this.btnDangKy.Name = "btnDangKy";
+            this.btnDangKy.Size = new System.Drawing.Size(150, 50);
+            this.btnDangKy.TabIndex = 89;
+            this.btnDangKy.Text = "      Xác nhận";
+            this.btnDangKy.UseVisualStyleBackColor = false;
             // 
             // FTaiKhoan
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 28F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.ClientSize = new System.Drawing.Size(1942, 1095);
+            this.ClientSize = new System.Drawing.Size(1532, 782);
+            this.Controls.Add(this.pnlDoiPass);
+            this.Controls.Add(this.btnThemHinh);
+            this.Controls.Add(this.picAvatar);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.btnDoiMK);
+            this.Controls.Add(this.btnCapNhat);
+            this.Controls.Add(this.txtDisplayName);
             this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FTaiKhoan";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FTaiKhoan";
+            ((System.ComponentModel.ISupportInitialize)(this.picAvatar)).EndInit();
+            this.pnlDoiPass.ResumeLayout(false);
+            this.pnlDoiPass.PerformLayout();
+            this.fpnlChucNang.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Button btnCapNhat;
+        private Button btnDoiMK;
+        private TextBox txtDisplayName;
+        private TextBox txtUserName;
+        private PictureBox picAvatar;
+        private Button btnThemHinh;
+        private Panel pnlDoiPass;
+        private TextBox txtMatKhauMoiNhapLai;
+        private TextBox txtMatKhauMoi;
+        private TextBox txtMatKhau;
+        private Label lblNhapLaiMK;
+        private Label lblMatKhauMoi;
+        private Label lblMatKhau;
+        private FlowLayoutPanel fpnlChucNang;
+        private Button btnReset;
+        private Button btnDangKy;
     }
 }
